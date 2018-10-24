8aaaf110608ce304f24fe3a7cdabba9881679134
Nhut Chau <chauminhnhut90@gmail.com>
2018-10-24 10:50:31 +0700

    Lesson 3: DO simple login task on 7Sport page

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,39 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
     public static void main(String[] args) {
-        // System.out.println("Hello World!");
+
+        // SetUp Chrome drive Path
+        System.setProperty("webdriver.chrome.driver","/Users/macbook/Documents/KolabsLLC/chromedriver");
+
+        // Create new chrome drive (after this below code, a chrome will be created)
+        WebDriver driver = new ChromeDriver();
+
+        // Navigate chrome to this address
+        driver.get("http://dev.7sports.co");
+
+        /*
+        * 2 important step when working with selenium
+        *
+        * + Find Element
+        * + Do action on element that was found
+        *
+        * */
+
+        // Find element UserName
+        WebElement userName = ((ChromeDriver) driver).findElementById("username");
+
+        // Do action on element Username
+        userName.sendKeys("titeo");
+
+
+        WebElement passWord = ((ChromeDriver) driver).findElementById("password");
+        passWord.sendKeys("123123");
+
+        WebElement login = ((ChromeDriver) driver).findElementByXPath("//*[@id=\"app\"]/div/div/section/div/div/div/a/span");
+        login.click();
     }
 }
