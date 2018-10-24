9af6026242189cdf76faa6703e468faa75779623
Anh Pham <anhpham@kolabs.co>
2018-10-24 13:58:17 +0700

    Add Comment into Login Agent site

--- src/Main.java
+++ src/Main.java
@@ -8,15 +8,15 @@ public class Main {
         System.setProperty("webdriver.chrome.driver","C:\\Users\\Kolabs1808002\\Downloads\\chromedriver_win32\\chromedriver.exe");
         WebDriver driver= new ChromeDriver();
         driver.get("http://7stag.win68.net/");
-
+        //Input Username
         WebElement UserNameTxt = ((ChromeDriver) driver).findElementById("txtusername");
         UserNameTxt.sendKeys("anh5");
-
+        //Input Password
         WebElement PasswordTxt = ((ChromeDriver) driver).findElementById("txtpassword");
         PasswordTxt.sendKeys("789987");
-
+        //Click Login Button
         WebElement LoginButton = ((ChromeDriver) driver).findElementByXPath("/html/body/div/div/div/div/form/fieldset/button");
         LoginButton.click();
-        
+
     }
 }
